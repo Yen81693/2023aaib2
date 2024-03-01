c3f31723b1cb91fce0387717301600b19731b9bc
Yen81693 <114201603+Yen81693@users.noreply.github.com>
2024-03-01 11:33:20 +0800

    week02

---
+++ week02/week02-4.cpp
@@ -0,0 +1,18 @@
+class Solution{
+public:
+    string maximumOddBinaryNumber(string s)
+    {
+        int N = s.length();
+        int one = 0; //s裡面, 有幾個1呢? 等一下會慢慢數出來
+        for(int i=0; i<N; i++) // C/C++迴圈(有圓括號)
+        {
+            if(s[i]=='1') one += 1; //if(判斷)也有圓括號
+        }
+        //printf("N:%d one:%d ", N, one); //先印出來, 有幾個1
+        string ans; //用來放答案
+        for(int i=0; i<one-1; i++) ans += '1'; //有幾個1要放前面?
+        for(int i=0; i<N-one; i++) ans += '0'; //有幾個0要放前面?
+        ans += '1';//最後塞個1
+        return ans;
+    }
+};
